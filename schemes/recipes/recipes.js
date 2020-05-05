04b550c607bfcde177bfbc143a7c5d4e4b747602
Amber-Pittman <amber@amberpittman.com>
2020-05-04 21:10:04 -0400

    Recipe routers created

--- schemes/recipes/recipes.js
+++ schemes/recipes/recipes.js
@@ -5,7 +5,26 @@ const router = express.Router()
 
 router.get("/", async (req, res, next) => {
     try {
-        res.json(await db("recipes"))
+        const recipes = await db.getRecipes()
+        res.json(recipes)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const list = await db.getShoppingList(req.params.id)
+        res.json(list)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get("/instructions/:id", async (req, res, next) => {
+    try {
+        const instructions = await db.getInstructions(req.params.id)
+        res.json(instructions)
     } catch(err) {
         next(err)
     }
